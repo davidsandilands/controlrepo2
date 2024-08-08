7cbaeb315e93945c1a94052df9cb15aae5805862
David Sandilands <david.sandilands@perforce.com>
2024-08-08 23:23:45 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -39,9 +39,10 @@ class { 'puppet_data_connector':
 }
 
 node 'ip-10-138-1-209.eu-west-1.compute.internal' {
- class { 'splunk::enterprise': }
-   seed_password    => true,
-   password_hash    => '$6$jxSX7ra2SNzeJbYE$J95eTTMJjFr/lBoGYvuJUSNKvR7befnBwZUOvr/ky86QGqDXwEwdbgPMfCxW1/PuB/IkC94QLNravkABBkVkV1',
+ class { 'splunk::enterprise': 
+  seed_password    => true,
+  password_hash    => '$6$jxSX7ra2SNzeJbYE$J95eTTMJjFr/lBoGYvuJUSNKvR7befnBwZUOvr/ky86QGqDXwEwdbgPMfCxW1/PuB/IkC94QLNravkABBkVkV1',
+  }
 }
 
 
